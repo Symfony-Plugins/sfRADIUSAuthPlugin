4f03f9cb60cd6281271755b9900059dca09b6dde
BigBadBassMan <BigBadBassMan@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-12-20 04:18:52 -0600

    initial version of the plugin, RADIUS auth via MSCHAPv2 is working
    git-svn-id: http://svn.symfony-project.com/plugins/sfRADIUSAuthPlugin/trunk@31694 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfRADIUSAuthPluginConfiguration.class.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Config file for sfRADIUSAuthPlugin
+ *
+ * PHP version 5
+ *
+ * @category   Symfony
+ * @package    Plugins
+ * @subpackage Auth
+ * @author     BigBadBassMan <d.reiche@gmx.ch>
+ * @license    http://www.symfony-project.org/license MIT
+ * @version    SVN: $Id:$
+ * @link       www.symfony-project.org
+ */
+
+/**
+ * Config class for sfRADIUSAuthPlugin
+ *
+ * @category   Symfony
+ * @package    Plugins
+ * @subpackage Auth
+ * @author     BigBadBassMan <d.reiche@gmx.ch>
+ * @license    http://www.symfony-project.org/license MIT
+ * @release    0.1.0
+ * @link       www.symfony-project.org
+ * @see        sfPluginConfiguration
+ */
+class sfRADIUSAuthPluginConfiguration extends sfPluginConfiguration
+{
+	/**
+	 * init method
+	 *
+	 * @see sfPluginConfiguration::initialize()
+	 *
+	 * @return void
+	 */
+	public function initialize()
+	{
+		if (!sfConfig::get('settings_sfRADIUSAuth_enabled', true)) {
+			return;
+		}
+
+		if (in_array('sfGuardAuth', sfConfig::get('settings_enabled_modules', array()))) {
+			sfConfig::set(
+				'app_sf_guard_plugin_check_password_callable',
+				array('sfRADIUSAuth', 'authenticateUser')
+			);
+		}
+
+	}
+
+	/**
+	 * initAutoload
+	 *
+	 * @see sfPluginConfiguration::initializeAutoload()
+	 *
+	 * @return void
+	 */
+	public function initializeAutoload()
+	{
+
+
+		parent::initializeAutoload();
+	}
+}
---
+++ lib/sfRADIUSAuth.class.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * RADIUS Auth plugin class for symfoyn
+ *
+ * PHP version 5
+ *
+ * @category   Symfony
+ * @package    Plugins
+ * @subpackage Auth
+ * @author     BigBadBassMan <d.reiche@gmx.ch>
+ * @license    http://www.symfony-project.org/license MIT
+ * @link       www.symfony-project.org
+ */
+
+/**
+ * RADIUS authentication class for symfony.
+ *
+ * @category   Symfony
+ * @package    Plugins
+ * @subpackage Auth
+ * @author     BigBadBassMan <d.reiche@gmx.ch>
+ * @license    http://www.symfony-project.org/license MIT
+ * @link       www.symfony-project.org
+ */
+class sfRADIUSAuth
+{
+	private $_radius;
+	private $_chap;
+	private static $_instance;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @return sfRADIUSAuthPlugin
+	 */
+	private function __construct()
+	{
+
+
+	}
+
+
+	/**
+	 * authenticate a username against a RADIUS server
+	 *
+	 * @param string $username username
+	 * @param string $password password
+	 *
+	 * @return boolean
+	 */
+	public static function authenticateUser($username, $password)
+	{
+		$instance = self::getInstance();
+
+		$type = sfConfig::get('app_sfRADIUSAuth_auth_type');
+		$server = sfConfig::get('app_sfRADIUSAuth_server');
+		$ports = sfConfig::get('app_sfRADIUSAuth_port');
+		$secret = sfConfig::get('app_sfRADIUSAuth_secret');
+
+		$class = 'Auth_RADIUS_'.$type;
+
+		$radius = new $class($username, $password);
+
+		switch ($type) {
+		case 'MSCHAPv2':
+			$radius->addServer($server, $ports['auth'], $secret);
+			$radius->username = $username;
+
+			$crpt = new Crypt_CHAP_MSv2();
+			$crpt->password = $password;
+			$crpt->username = $username;
+
+			$radius->challenge = $crpt->authChallenge;
+			$radius->peerChallenge = $crpt->peerChallenge;
+			$radius->chapid = $crpt->chapid;
+			$radius->response = $crpt->challengeResponse();
+
+			break;
+		case 'MSCHAPv1':
+			break;
+		case 'PAP':
+			break;
+
+		case 'CHAP_MD5':
+			break;
+		}
+
+		if (!$radius->start()) {
+			throw new sfException($radius->getError());
+			return false;
+		}
+
+		$result = $radius->send();
+		if (PEAR::isError($result)) {
+			$retval = false;
+		} elseif ($result === true) {
+			$retval = true;
+		} else {
+			$retval = false;
+		}
+
+		$radius->close();
+		return $retval;
+	}
+
+	/**
+	 * Singleton.
+	 *
+	 * @static
+	 *
+	 * @return void
+	 */
+	public static function getInstance()
+	{
+		if (self::$_instance !== null) {
+			return self::$_instance;
+		}
+
+		self::$_instance = new sfRADIUSAuth();
+		return self::$_instance;
+	}
+}
